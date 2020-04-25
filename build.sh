402fb72c99348c78661081dbcafd7a819c43c0f2
Marcus Rohrmoser <work@mro.name>
2020-04-25 15:21:20 +0200

    build++

--- build.sh
+++ build.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
+# get the go toolchain https://golang.org/dl/
 # https://golang.org/doc/install/source#environment
 #
 
 cd "$(dirname "${0}")" || exit 1
-# $ uname -s -m
-# Darwin x86_64
-# Linux x86_64
-# Linux armv6l
 
-say="say"
+if say -v '?' 1>/dev/null 2>/dev/null ; then
+  say="say"
+else
+  say="echo"
+fi
+
+# prevent GOPATH pointing outside userland (especially on Alpine)
+go env -w GOPATH="${HOME}/go"
+mkdir -p "${GOPATH}" 2>/dev/null
+go env -w GOBIN="$(go env GOPATH)/bin"
+
 parm="" # "-u"
 {
-  "${say}" "go get"
+  "${say}" "go get (install dependencies)"
   go get "${parm}" github.com/gorilla/sessions \
     github.com/jteeuwen/go-bindata/... \
     golang.org/x/crypto/bcrypt \
@@ -20,13 +27,12 @@ parm="" # "-u"
     golang.org/x/text/language \
     golang.org/x/text/search \
     gopkg.in/yaml.v2 \
-    \
     github.com/stretchr/testify \
     github.com/yhat/scrape \
     golang.org/x/tools/blog/atom
 }
 
-"$(go env GOPATH)/bin/go-bindata" -ignore="\\.DS_Store" -ignore="assets/dark/" -ignore=".+\\.woff" -prefix static static/... tpl/...
+"$(go env GOBIN)/go-bindata" -ignore="\\.DS_Store" -ignore="assets/dark/" -ignore=".+\\.woff" -prefix static static/... tpl/...
 
 VERSION="$(grep -F 'version = ' version.go | cut -d \" -f 2)"
 LDFLAGS="-s -w -X main.GitSHA1=$(git rev-parse --short HEAD)"
@@ -35,7 +41,9 @@ rm shaarligo-*.cgi 2>/dev/null
 
 "${say}" "test"
 umask 0022
-go fmt && go vet && go test --short || { exit $?; }
+go fmt || { exit $?; }
+go vet || { exit $?; }
+go test || { exit $?; }
 "${say}" "ok"
 
 "${say}" "build localhost"
@@ -56,7 +64,7 @@ mkdir -p "build/${VERSION}-Linux-armv6l/"
 env GOOS=linux GOARCH=amd64       go build -ldflags="${LDFLAGS}" -o "build/${VERSION}-Linux-x86_64/shaarligo.cgi" || { echo "Aua" 1>&2 && exit 1; }
 env GOOS=linux GOARCH=arm GOARM=6 go build -ldflags="${LDFLAGS}" -o "build/${VERSION}-Linux-armv6l/shaarligo.cgi" || { echo "Aua" 1>&2 && exit 1; }
 
-"${say}" "s0"
+"${say}" "deploy to dev server"
 
 if [ "${1}" = "prod" ] ; then
   rsync -avPz "build/" s0:"/var/www/lighttpd/darknet.mro.name/public_html/dev/shaarligo/"
