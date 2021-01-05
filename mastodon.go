8430460b30883ffd87a07941bbe1b280d0424c74
Marcus Rohrmoser <work@mro.name>
2021-01-05 17:56:32 +0100

    Copyright

--- mastodon.go
+++ mastodon.go
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2020-2020 Marcus Rohrmoser, http://purl.mro.name/ShaarliGo
+// Copyright (C) 2020-2021 Marcus Rohrmoser, http://purl.mro.name/ShaarliGo
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
